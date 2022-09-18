0aa302749c44139c0603660159d97e19c2cceea9
Joey Pettit <pettitbass@gmail.com>
2022-09-18 15:47:25 -0500

    created basic list construciton in with jq

--- server/public/client.js
+++ server/public/client.js
@@ -8,15 +8,26 @@ function onReady(){
     console.log('js and jQ loaded');
 
     // click handlers
+    $('.task').on('click', toggleCollapse);
 
    // load current list from server
 
     //
 
+}
+
+function toggleCollapse(event){
+    // store task id as variable
+    let taskId = $(event.target).closest('li').data('taskid');
+
+    // toggle visibility of notes when button clicked
+    $(event.target).parent().siblings('.collapsible').toggle();
+    
+
+
 
 
 }
-
 
 
 
