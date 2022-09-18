d121bbf00b786ec55991c95a1391c11b9d1b801a
Joey Pettit <pettitbass@gmail.com>
2022-09-17 23:10:22 -0500

    added feature in PUT, remove date_completed if isComplete is false

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
         // catch error
         console.log('Error SELECTing from database', error);
         res.sendStatus(500);
-    })
+    });
 });//END router.get
 
 // Create POST endpoint to create new task
@@ -55,24 +55,61 @@ router.delete('/:taskid', (req, res) => {
     }).catch((error) => {
         console.log('Error with DELETE', error);
         res.sendStatus(500);
-    })
+    });
 }); // END router.delete
+
 
 // Create PUT endpoint to toggle is_complete
 router.put('/toggleComplete/:taskid', (req, res) => {
      // assign req.params. taskid to variable
     let toToggleId = req.params.taskid;
-    console.log('In PUT (toggle). Task to be toggled:', toToggleId);
-    // query to send
-    let queryText = `UPDATE "tasks" SET "is_complete"= (NOT "is_complete") WHERE "id"=$1;`;
+    // assign date completed to variable
+    let dateCompleted = req.body.dateCompleted;
+    console.log('In PUT (toggle). Task to be toggled:', toToggleId,'Date:', dateCompleted);
 
-    pool.query(queryText, [toToggleId])
+    // capture current state of isComplete,
+    // convert from string to boolean and toggle
+    let isComplete = (req.body.isComplete === 'true');
+    console.log('isComplete after toggle', isComplete);
+
+    // if isComplete is false, set dateCompleted to NULL
+    if(isComplete === false){
+        dateCompleted = null;
+    }
+
+    // query to send
+    let queryText = `UPDATE "tasks" SET "is_complete"= $2, "date_completed"=$3 WHERE "id"=$1;`;
+
+    pool.query(queryText, [toToggleId, isComplete, dateCompleted])
     .then(() => {
         console.log('Toggle complete sucessful');
-    })catch((error) => {
+        res.sendStatus(200);
+    }).catch((error) => {
         console.log('Error with PUT(toggle)', error);
         res.sendStatus(500);
-    })
+    });
+}); // END router.put (toggle)
+
+
 // Create PUT endpoint to edit existing task
+// router.put('/:taskid', (req, res) => {
+//     // assign req.params.taskid and payload to variables
+//    let toToggleId = req.params.taskid;
+//    let {taskName, isComplete, dateCompleted, dueDate, notes} = req.body;
+
+//    console.log('In PUT (toggle). Task to be edited:', toToggleId);
+//    // query to send, toggle of value happens in query command
+//    let queryText = `UPDATE "tasks" SET "is_complete"= (NOT "is_complete") WHERE "id"=$1;`;
+
+//    pool.query(queryText, [toToggleId])
+//    .then(() => {
+//        console.log('Toggle complete sucessful');
+//        res.sendStatus(200);
+//    }).catch((error) => {
+//        console.log('Error with PUT(toggle)', error);
+//        res.sendStatus(500);
+//    });
+// }); // end PUT (edit task)
+
 
 module.exports = router;
