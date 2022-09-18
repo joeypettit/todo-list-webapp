ff8fd98652aa555bf7552ad406a8a13748e0b5aa
Joey Pettit <pettitbass@gmail.com>
2022-09-17 21:07:59 -0500

    POST router made/tested

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -18,11 +18,27 @@ router.get('/', (req, res) => {
         console.log('Error SELECTing from database', error);
         res.sendStatus(500);
     })
-
-
 });//END router.get
 
 // Create POST endpoint to create new task
+router.post('/', (req, res) => {
+    // send new task from client, save as variable
+    // new task is an object {taskName, complete?, dueDate, dateCompleted, extraNotes}
+    let {taskName, isComplete, dateCompleted, dueDate, notes} = req.body;
+    console.log('in POST endpoint. Task to add is:', taskName, isComplete, dateCompleted, dueDate, notes);
+    const queryText = ` INSERT INTO "tasks" ("task_name", "is_complete", "due", "date_completed","notes")
+                                    VALUES	($1, $2, $3, $4, $5);`
+
+    pool.query(queryText, [taskName, isComplete, dueDate, dateCompleted, notes])
+    .then(() => {
+        console.log('POST Successful');
+        res.sendStatus(201);
+
+    }).catch((error) => {
+        console.log('Error in POST:', error);
+
+    });
+});
 
 // Create DELETE endpoint to delete a task
 
